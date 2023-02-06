5f25d15448639e3f95f4c06b7edc5b59704b88bc
xcbhx <ceina.ellison@gmail.com>
2023-02-06 14:30:04 -0500

    edit page

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@ const navMenu = document.getElementById("nav-menu"),
       navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
     }
   }
+
+// Header scroll animation
+window.addEventListener("scroll", ()=> {
+  if(window.scrollY > 40) {
+  header.classList.add("header--scroll");
+  } else {
+    header.classList.remove("header--scroll");
+  }
+});
+
+// ScrollReveal animations
+const sr = ScrollReveal({
+  duration: 2000,
+  distance: "100px",
+  delay: 400,
+  reset: true,
+});
+
+sr.reval(".hero__content, .about__content");
+sr.reveal(".hero__img", { origin: "top" });
+
+sr.reveal(
+  ".hero__infor-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .project__content, .footer__content", 
+  {
+  delay: 500,
+  interval: 100,
+});
+
+sr.reveal("qualification__footer-text, . contact__content", {
+  origin: "left",
+});
+
+sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
