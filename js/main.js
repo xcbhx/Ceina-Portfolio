9dd7b02f60b794689b3e93fff776976e7ed40eaf
xcbhx <ceina.ellison@gmail.com>
2023-02-18 11:24:42 -0500

    add/edit sections

--- js/main.js
+++ js/main.js
@@ -3,37 +3,36 @@ const navMenu = document.getElementById("nav-menu"),
   navItem = document.querySelectorAll(".nav__item"),
   header = document.getElementById("header");
 
+// open and close menu
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu--open");
+  changeIcon();
+});
 
-  // open and close menu
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav__menu--open");
+// close the menu when the user clicks the nav links
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav__menu--open")) {
+      navMenu.classList.remove("nav__menu--open");
+    }
     changeIcon();
   });
+});
 
-
-  // close the menu when the user clicks the nav links
-  navItem.forEach((item) => {
-    item.addEventListener("click", () => {
-      if (navMenu.classList.contains("nav__menu--open")) {
-        navMenu.classList.remove("nav__menu--open");
-      }
-      changeIcon();
-    });
-  });
-
-  //Change nav toggle icon 
-  function changeIcon() {
-    if (navMenu.classList.contains("nav__menu--open")) {
-      navToggle.classList.replace("ri-menu-3-line", "ri-close-line");
-    } else {
-      navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
-    }
+// Change nav toggle icon
+function changeIcon() {
+  if (navMenu.classList.contains("nav__menu--open")) {
+    navToggle.classList.replace("ri-menu-3-line", "ri-close-line");
+  } else {
+    navToggle.classList.replace("ri-close-line", "ri-menu-3-line");
   }
+}
 
-// Header scroll animation
-window.addEventListener("scroll", ()=> {
-  if(window.scrollY > 40) {
-  header.classList.add("header--scroll");
+
+// header scroll animation
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("header--scroll");
   } else {
     header.classList.remove("header--scroll");
   }
@@ -44,20 +43,21 @@ const sr = ScrollReveal({
   duration: 2000,
   distance: "100px",
   delay: 400,
-  reset: true,
+  reset: false,
 });
 
-sr.reval(".hero__content, .about__content");
+sr.reveal(".hero__content, .about__content");
 sr.reveal(".hero__img", { origin: "top" });
 
 sr.reveal(
-  ".hero__infor-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .project__content, .footer__content", 
+  ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .service__card, .project__content, .testimonial__wrapper, .footer__content",
   {
-  delay: 500,
-  interval: 100,
-});
+    delay: 500,
+    interval: 100,
+  }
+);
 
-sr.reveal("qualification__footer-text, . contact__content", {
+sr.reveal(".qualification__footer-text, .contact__content", {
   origin: "left",
 });
 
